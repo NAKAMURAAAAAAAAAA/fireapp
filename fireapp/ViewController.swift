2bded7e2a9a5d8a258a510bb995c6da94ef5de48
Kan Nakamura <kan.nakamura0711@gmail.com>
2019-11-16 23:17:14 +0900

    Initial Commit

---
+++ fireapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fireapp
+//
+//  Created by Kan Nakamura on 2019/11/16.
+//  Copyright © 2019 Kan Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
